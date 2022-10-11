9ccf09015be7e61ed4d183e13e143bc7a90b8ad5
aldyfikhrimuhammad <aldyfikhrimuhammad@gmail.com>
2022-10-11 15:36:44 +0700

    Finish

--- Latihan-html/script.js
+++ Latihan-html/script.js
@@ -1,6 +1,6 @@
 console.log("Tugas Logic Javascript")
 
-// Nomor 1 - n = 9, x === y
+// Nomor 1
 function nomorSatu(n) {
     let hasil = "";
     for (let i = 0; i < n; i++) {
@@ -19,25 +19,26 @@ function nomorSatu(n) {
 console.log("Nomor 1 :");
 console.log(nomorSatu(9));
 
-// Nomor 3 - n = 9, x === y
-// function nomorTiga(n) {
-//     let hasil = "";
-//     for (let i = 0; i < n; i++) {
-//         for (let j = 0; j < n; j++) {
-//             if (i === j && j === i) {
-//                 hasil += "*";
-//             } else {
-//                 hasil += " ";
-//             }
-//         }
-//         hasil += "\n";
-//     }
-//     return hasil;
-// }
+// Nomor 3
+function nomorTiga(n) {
+    let hasil = "";
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            if (i === j || j === (n - 1 - i)) {
+                hasil += "*";
+            } else {
+                hasil += " ";
+            }
+        }
+        hasil += "\n";
+    }
+    return hasil;
+}
 
-// console.log(nomorTiga(9));
+console.log("Nomor 3 :");
+console.log(nomorTiga(9));
 
-// Nomor 5 - n = 9, x === y
+// Nomor 5
 function nomorLima(n) {
     let hasil = "";
     for (let i = 0; i < n; i++) {
@@ -56,6 +57,23 @@ console.log("Nomor 5 :");
 console.log(nomorLima(9));
 
 // Nomor 7
+function nomorTujuh(n) {
+    let hasil = "";
+    for (let i = n; i > 0; i--) {
+        for (let j = 1; j <= n; j++) {
+            if (j >= i) {
+                hasil += "*";
+            } else {
+                hasil += "";
+            }
+        }
+        hasil += '\n';
+    }
+    return hasil;
+}
+
+console.log("Nomor 7 :");
+console.log(nomorTujuh(10));
 
 // Nomor 9
 function nomorSembilan(n) {
@@ -94,6 +112,4 @@ function nomorSembilan(n) {
 }
 
 console.log("Nomor 9 :");
-console.log(nomorSembilan(9));
-
-
+console.log(nomorSembilan(5));
